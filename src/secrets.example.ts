7f65a12f0727df0b41c17c2639962cc360ca48ee
Samuel Gratzl <sam@sgratzl.com>
2021-12-30 13:02:44 -0500

    fix: build error

---
+++ src/secrets.example.ts
@@ -0,0 +1,13 @@
+export default {
+  key: 'SECRET KEY',
+  ecdsa: {
+    public: {
+      crv: 'SECRET',
+      ext: true,
+      key_ops: ['verify'],
+      kty: 'EC',
+      x: 'SECRET',
+      y: 'SECRET',
+    },
+  },
+};
